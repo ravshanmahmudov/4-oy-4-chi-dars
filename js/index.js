734e449cb22245b6bca2379696d98890551a4dff
ravshanmahmudov <ravshanmahmudov882@gmail.com>
2024-05-24 08:07:45 +1400

    masalar

---
+++ js/index.js
@@ -0,0 +1,75 @@
+// 1chi masala
+
+// alert("vaqt")
+// let t = 23;
+// let T = 1000;
+// let g = (t+T)%24;
+// let a = +prompt("a")
+// console.log(g);
+
+// 2chi masala
+// let C = +prompt('gradusni selsiy shkalasida kiriting');
+// let = k;
+// let = f;
+// k = C + 273.15;
+// k = C * 1.80 + 32.00;
+// console.log(k);
+// console.log(f);
+
+// 3chi masala
+
+// alert("bilmasvoy")
+// let v = +prompt("v");
+// let a =  +prompt("a");
+// let b =  +prompt("b");
+// let g = (b-v);
+// let k = (a-v);
+// console.log(g);
+// console.log(k);
+
+// 4chi masala
+// alert("tarvuz")
+// let n = +prompt("n");
+// let a =  +prompt("a");
+// let b = (n % a);
+// console.log(b);
+
+// 5chi masala
+// alert("tomonlari")
+// let n = +prompt("n");
+// let b = n * (n-2); 
+// console.log(b);
+
+// 7chi masala
+// alert("vaqt")
+// let b = +prompt("b");
+// let s =  +prompt("s");
+// let y = (b-s);
+// let h = (y/2)
+// console.log(h);
+
+// 8chi masala
+// alert("vaqt")
+// let b = +prompt("b");
+// let t =  +prompt("t");
+// let y = (b+t)/2;
+// console.log(y);
+
+// 9chi masala
+// alert("tomonlar")
+// let n = +prompt("n");
+// let b = n * (n-3)/2; 
+// console.log(b);
+
+
+
+// 10chi masala
+// alert("tomonlar")
+// let T = +prompt("T");
+// let A = +prompt("A");
+// let B = +prompt("B ");
+// let b = (T - A)/(B + A); 
+// console.log(b);
+
+
+
